b94bad509e4e144a24e321d5cd5a29e570a86315
khaleelibraheem <alhajikhaleel@gmail.com>
2025-12-01 04:00:02 +0530

    fix card layout

--- app/shop/page.js
+++ app/shop/page.js
@@ -26,7 +26,7 @@ export default function ShopPage() {
         </div>
 
         {/* Product Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-10">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-10">
            {products.map((product) => (
              <Link key={product.id} href={`/product/${product.slug}`}>
                 <ProductCard 
